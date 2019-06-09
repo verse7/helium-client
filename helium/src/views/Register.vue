3b2d0c880b7952c334a208722a4145087a741c83
Kayon-Marie <k.marie.douglas@hotmail.com>
2019-06-09 16:56:15 -0400

    vue cli setup

---
+++ helium/src/views/Register.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>This is the register page</h1>
+  </div>
+</template>
---
+++ helium/src/views/Subscriptions.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>This is the subscriptions page</h1>
+  </div>
+</template>
